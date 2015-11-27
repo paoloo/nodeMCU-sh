c40481c979671d35338fd34956598cacfbe07dc9
Paolo Oliveira <paolo@lit.ifce.edu.br>
2015-11-26 23:39:06 -0300

    Update shell.lua

--- shell.lua
+++ shell.lua
@@ -1,27 +1,27 @@
 local shell = {}
 
-function shell.cat(arquivo)
+shell.cat = function(arquivo)
   file.open(arquivo,"r")
   print(file.read())
   file.close()
 end
 
-function shell.ls()
+shell.ls = function()
   for k,v in pairs(file.list()) do
     print(k,v)
   end
 end
 
-function shell.ip()
+shell.ip = function()
   print(wifi.sta.getip())
 end
 
-function shell.wifi(rede,senha)
+shell.wifi = function(rede,senha)
   wifi.setmode(wifi.STATION)
   wifi.sta.config(rede,senha)
 end
 
-function shell.lswifi()
+shell.lswifi = function()
   function listap(t)
     for k,v in pairs(t) do
       print(k.." : "..v)
@@ -30,15 +30,15 @@ function shell.lswifi()
   wifi.sta.getap(listap)
 end
 
-function shell.rm(arquivo)
+shell.rm = function(arquivo)
   file.remove(arquivo)
 end
 
-function shell.mv(org, dst)
+shell.mv = function(org, dst)
   file.rename(org, dst)
 end
 
-function shell.cp(org, dst)
+shell.cp = function(org, dst)
   file.open(org,"r")
   local conteudo = file.read()
   file.close()
@@ -47,9 +47,15 @@ function shell.cp(org, dst)
   file.close()
 end
 
-function shell.df()
+shell.df = function()
   _r, _u, _t=file.fsinfo()
   print("Size\tUsed\tAvail\n".._t.."\t".._u.."\t".._r.."\n")
+end
+
+shell.whois = function(url)
+  sk=net.createConnection(net.TCP, 0)
+  sk:dns(url,function(conn,ip) print(url.." is "..ip) end)
+  sk=nil
 end
 
 return shell
