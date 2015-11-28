18c0c7bb176c8c82b0e2a54646094ad986e6a86f
Paolo Oliveira <paolo@lit.ifce.edu.br>
2015-11-27 23:49:36 -0300

    Create wget.lua

---
+++ wget.lua
@@ -0,0 +1,18 @@
+-- tentativa de fazer um wget. Funciona para pequenos arquivios,tentar corrigir para qualquer tamanho
+
+wget = function(endereco,caminho,saida,porta)
+    porta = porta or 80
+    saida = saida or "output-wget"
+    print("salvando "..endereco..":"..porta..caminho.." em "..saida.."!")
+    file.open(saida, "w")
+    s=net.createConnection(net.TCP, 0)
+    s:on("receive", function(sck, c) file.write(c) file.flush() end )
+    s:on("disconnection", function(c) c = nil; file.close() end)
+    s:connect(porta, endereco)
+    s:send("GET "..caminho.." HTTP/1.1\r\n"..
+           "Host: "..endereco.."\r\n"..
+           "Connection: keep-alive\r\n"..
+           "Accept-Charset: utf-8\r\n"..
+           "Accept: */*\r\n\r\n"
+          )
+end
